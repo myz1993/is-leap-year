ddba5f3e1310df6a2b42c4ab90d55a20f0ff9bd7
llaobin0207 <llaobin0207@163.com>
2020-05-02 02:05:10 +0800

    判断一个年份是否是闰年

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -11,8 +11,13 @@ public class Main {
      */
     public static boolean isLeapYear(int year) {
         if (year < 0) return false;
-        if(year % 100 == 0 && year % 400 == 0){
-            return true;
+        if(year % 100 == 0 ){
+            if( year % 400 == 0){
+                return true;
+            }else{
+                return false;
+            }
+
         }else if(year % 4 == 0){
             return true;
         }else{
