adf01681ac3890b86c8b3f8658c2c4e432d1efd1
gaozhidong <1011284295@qq.com>
2020-10-09 14:55:33 +0800

    :sparkles: 判断是不是闰年

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -10,7 +10,7 @@ public class Main {
      * @return 如果该年份是闰年，返回true，否则返回false
      */
     public static boolean isLeapYear(int year) {
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
             return true;
         } else {
             return false;
