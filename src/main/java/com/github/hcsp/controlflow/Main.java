9ebc85298250ad3012ef8087ff35c0f5edfb88a5
Yangbisheng <2606424906@qq.com>
2021-08-05 21:28:25 +0800

    is leap year

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -13,6 +13,9 @@ public class Main {
         if ((year%400) == 0) {
             return true;
         }
+        if (((year%4) == 0) && ((year%100) != 0)) {
+            return true;
+        }
         return false;
     }
 
