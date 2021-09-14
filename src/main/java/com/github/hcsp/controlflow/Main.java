c0bd593d9fb0b6b14d460eaddee885c6ec327a31
leo.liu <shixun.liu1023@gmail.com>
2021-09-14 13:43:49 +1000

    change

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -10,7 +10,7 @@ public class Main {
      * @return 如果该年份是闰年，返回true，否则返回false
      */
     public static boolean isLeapYear(int year) {
-        if (year % 100 == 0 ){
+        if (year % 100 == 0) {
             if (year % 400 == 0) {
                 return true;
             }
